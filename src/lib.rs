b13970eebd2eede9fb7b8ae923a78c7780c657af
timglabisch <tim@ainfach.de>
2016-02-08 22:15:23 +0100

    init

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::fmt;
+
+trait Yolo<T> {
+    fn yolo(self) -> T;
+}
+
+impl<T> Yolo<T> for Option<T> {
+    fn yolo(self) -> T {
+        self.unwrap()
+    }
+}
+
+impl<T, E> Yolo<T> for Result<T, E> where E : fmt::Debug {
+    fn yolo(self) -> T {
+        self.unwrap()
+    }
+}
+
+fn get_result(v : i32) -> Result<i32, ()> {
+    Ok(v)
+}
+
+#[test]
+fn it_works() {
+    assert_eq!(Some(123).yolo(), 123);
+    assert_eq!(get_result(123).yolo(), 123)
+}
